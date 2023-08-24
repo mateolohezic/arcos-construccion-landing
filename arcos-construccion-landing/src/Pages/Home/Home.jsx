332b886f1c52ea83ccd33ba1bcd2e80e1092b28f
Mateo Lohezic <mateolohezic@gmail.com>
2023-08-24 20:13:35 -0300

    Project Created

---
+++ arcos-construccion-landing/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <>
+        Hola como estas? Hola como estas?Hola como estas? Hola como estas?Hola como estas? Hola como estas?Hola como estas? Hola como estas?Hola como estas? Hola como estas?Hola como estas? Hola como estas?Hola como estas? Hola como estas?
+    </>
+  )
+}
+
+export default Home
